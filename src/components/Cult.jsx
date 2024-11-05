8b1b6c54fba468fc6f10f2d060186e5a47c3fbef
shamilshiraz <shamilshiraz0@gmail.com>
2024-11-05 21:52:22 +0530

    second

--- src/components/Cult.jsx
+++ src/components/Cult.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 function Cult() {
   return (
-<div className="w-screen  flex justify-center  text-center font-size-2xl">
+<div className="w-100  flex-col  justify-center  text-center text-9xl">
 <h2>The Cult</h2>
+<div class="w-3/4 grid grid-cols-3 gap-6 mx-auto self-center">  <img src="haed.jpg" alt="Post 1" class="w-full h-auto object-cover"/>
+  <img src="babyblonde.jpg" alt="Post 2" class="w-full h-auto object-cover"/>
+  <img src="dan.jpg" alt="Post 3" class="w-full h-auto object-cover"/>
+  <img src="lost.jpg" alt="Post 4" class="w-full h-auto object-cover"/>
+  <img src="GR.jpg" alt="Post 5" class="w-full h-auto object-cover"/>
+  <img src="mac.jpg" alt="Post 6" class="w-full h-auto object-cover"/>
+  <img src="scream.jpg" alt="Post 7" class="w-full h-auto object-cover"/>
+  <img src="nash.jpg" alt="Post 8" class="w-full h-auto object-cover"/>
+  <img src="whozayn.jpg" alt="Post 9" class="w-full h-auto object-cover"/>
+  <img src="mubass.jpg" alt="Post 10" class="w-full h-auto object-cover"/>
+
+</div>
 
     </div>
   )
