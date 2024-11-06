185332bdbf8ef53d750d8b962d8129651ef9467f
shamilshiraz <shamilshiraz0@gmail.com>
2024-11-06 06:20:30 +0530

    second

--- src/components/Cult.jsx
+++ src/components/Cult.jsx
@@ -4,7 +4,7 @@ function Cult() {
   return (
 <div className="w-100  flex-col  justify-center  text-center text-9xl mt-[15%]">
 <h2>The Cult</h2>
-<div class="lg-w-3/4 grid grid-cols-3 gap-6 mx-auto self-center">  <img src="haed.jpg" alt="Post 1" class="w-full h-auto object-cover"/>
+<div class="lg:w-3/4 grid grid-cols-3 gap-6 mx-auto self-center">  <img src="haed.jpg" alt="Post 1" class="w-full h-auto object-cover"/>
   <img src="babyblonde.jpg" alt="Post 2" class="w-full h-auto object-cover"/>
   <img src="dan.jpg" alt="Post 3" class="w-full h-auto object-cover"/>
   <img src="lost.jpg" alt="Post 4" class="w-full h-auto object-cover"/>
